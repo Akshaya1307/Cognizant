9ae1b39f9dba825c4dfec7ddbfe7df814a074439
Naga Akshaya Boyidi <boyidinagaakshaya@gmail.com>
2026-07-20 20:37:18 +0530

    Delete Digital-Nurture-DotNetFSE-main/DotNet FSE/Deepskilling/Module-06-ASP.NET-Core-8.0 directory

--- Digital-Nurture-DotNetFSE-main/DotNet FSE/Deepskilling/Module-06-ASP.NET-Core-8.0/DTOs/ProductDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace RetailInventory.DTOs
-{
-    public class ProductDTO
-    {
-        public string Name { get; set; } = string.Empty;
-
-        public string CategoryName { get; set; } = string.Empty;
-    }
-}
